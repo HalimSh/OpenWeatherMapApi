71a2c1e8acc3ecd09f0207ee0b7acce5efb94e4e
MahamudHub <mo.mahamud@outlook.com>
2020-07-28 14:34:23 +0100

    POJO classes by name commit

---
+++ src/test/java/sparta/eng61/com/POJOByName/Sys.java
@@ -0,0 +1,53 @@
+
+package sparta.eng61.com.POJOByName;
+
+public class Sys {
+
+
+    private String country;
+    private Long id;
+    private Long sunrise;
+    private Long sunset;
+    private Long type;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(Long sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public Long getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(Long sunset) {
+        this.sunset = sunset;
+    }
+
+    public Long getType() {
+        return type;
+    }
+
+    public void setType(Long type) {
+        this.type = type;
+    }
+
+}
---
+++ src/test/java/sparta/eng61/com/POJOByName/Wind.java
@@ -0,0 +1,26 @@
+
+package sparta.eng61.com.POJOByName;
+
+public class Wind {
+
+
+    private Long deg;
+    private Double speed;
+
+    public Long getDeg() {
+        return deg;
+    }
+
+    public void setDeg(Long deg) {
+        this.deg = deg;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+}
