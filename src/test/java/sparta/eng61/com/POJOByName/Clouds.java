48eddedcb8ba51bf9e443aea22ad55f7a384a630
MahamudHub <mo.mahamud@outlook.com>
2020-07-28 14:34:23 +0100

    POJO classes by name commit

---
+++ src/test/java/sparta/eng61/com/POJOByName/Clouds.java
@@ -0,0 +1,17 @@
+
+package sparta.eng61.com.POJOByName;
+
+public class Clouds {
+
+
+    private Long all;
+
+    public Long getAll() {
+        return all;
+    }
+
+    public void setAll(Long all) {
+        this.all = all;
+    }
+
+}
---
+++ src/test/java/sparta/eng61/com/POJOByName/Main.java
@@ -0,0 +1,63 @@
+
+package sparta.eng61.com.POJOByName;
+
+
+public class Main {
+
+
+    private Double feelsLike;
+    private Long humidity;
+    private Long pressure;
+    private Double temp;
+    private Double tempMax;
+    private Double tempMin;
+
+    public Double getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(Double feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public Long getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Long humidity) {
+        this.humidity = humidity;
+    }
+
+    public Long getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(Long pressure) {
+        this.pressure = pressure;
+    }
+
+    public Double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Double temp) {
+        this.temp = temp;
+    }
+
+    public Double getTempMax() {
+        return tempMax;
+    }
+
+    public void setTempMax(Double tempMax) {
+        this.tempMax = tempMax;
+    }
+
+    public Double getTempMin() {
+        return tempMin;
+    }
+
+    public void setTempMin(Double tempMin) {
+        this.tempMin = tempMin;
+    }
+
+}
