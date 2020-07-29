e483991c29e95445ce18f020c5c01fde2a17e8bc
Raja Roy chowdhury <raja@rajas-mbp.home>
2020-07-29 15:08:09 +0100

    Adding a few tests that I used to make sure the HeadersDTO was working.

--- src/test/java/sparta/eng61/com/AppTest.java
+++ src/test/java/sparta/eng61/com/AppTest.java
@@ -1,20 +1,38 @@
 package sparta.eng61.com;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    HeadersDTO headersDTO = new HeadersDTO("HTTP URL");
+    CityNameDTO cityNameDTO = new CityNameDTO("HTTP URL");
+
     /**
      * Rigorous Test :-)
      */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @DisplayName("Checking the Day, Date and time of when the request was made.")
+    public void dayDateTimeTest(){
+        Assertions.assertEquals("openresty",headersDTO.getServer());
+    }
+
+    @Test
+    @DisplayName("Checking Longitude")
+    public void lonTest(){
+        Assertions.assertEquals(-0.13,cityNameDTO.getCroodL());
     }
 }
