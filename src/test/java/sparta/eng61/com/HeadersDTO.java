8b44841af62783a7458f640cad636ff4f35c482f
Raja Roy chowdhury <raja@rajas-mbp.home>
2020-07-29 11:27:05 +0100

    Updating HeadersDTO class with all methods to call Methods for checking Headers.

--- src/test/java/sparta/eng61/com/HeadersDTO.java
+++ src/test/java/sparta/eng61/com/HeadersDTO.java
@@ -1,4 +1,50 @@
 package sparta.eng61.com;
 
+import java.net.HttpURLConnection;
+
 public class HeadersDTO {
+
+    private HttpURLConnection httpURLConnection;
+
+    public HeadersDTO(String link) {
+        ConnectionManager connect = new ConnectionManager();
+        httpURLConnection = connect.readAPIHeaders(link);
+    }
+
+    public String getServer() {
+        return httpURLConnection.getHeaderField("Server");
+    }
+
+    public String getDayDateTime() {
+        return httpURLConnection.getHeaderField("Date");
+    }
+
+    public String getContentType() {
+        return httpURLConnection.getHeaderField("Content-Type");
+    }
+
+    public String getContentlength() {
+        return httpURLConnection.getHeaderField("Content-Length");
+    }
+
+    public String getConnection() {
+        return httpURLConnection.getHeaderField("Connection");
+    }
+
+    public String getXCacheKey(){
+        return httpURLConnection.getHeaderField("X-Cache-Key");
+    }
+
+    public String getAccessControlAllowOrigin(){
+        return httpURLConnection.getHeaderField("Access-Control-Allow-Origin");
+    }
+
+    public String getAccessControlAllowCredentials(){
+        return httpURLConnection.getHeaderField("Access-Control-Allow-Credentials");
+    }
+
+    public String getAccessControlAllowMethods(){
+        return httpURLConnection.getHeaderField("Access-Control-Allow-Methods");
+    }
+
 }
