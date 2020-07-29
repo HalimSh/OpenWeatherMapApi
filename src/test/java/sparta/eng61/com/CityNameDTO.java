4d2b437b7c5904c5e4069297f3fff2bad59feff6
MahamudHub <mo.mahamud@outlook.com>
2020-07-29 13:01:35 +0100

    DTO Byname update commit

--- src/test/java/sparta/eng61/com/CityNameDTO.java
+++ src/test/java/sparta/eng61/com/CityNameDTO.java
@@ -2,8 +2,6 @@ package sparta.eng61.com;
 
 import sparta.eng61.com.POJOByName.*;
 
-import java.awt.*;
-
 public class CityNameDTO {
 
     private CityNamePOJO cityNamePOJO;
@@ -16,24 +14,105 @@ public class CityNameDTO {
 
     // Coord Getters
     public double getCroodLat() {
-        return (Double) cityNamePOJO.getCoord().getLat();
+        return cityNamePOJO.getCoord().getLat();
     }
 
-    public double getCroodL() {
-        return (Double) cityNamePOJO.getCoord().getLon();
+    public double getCroodLon() {
+        return cityNamePOJO.getCoord().getLon();
     }
 
     // Weather Getters
-//    public List getWeatherID(){
-//        return (List) cityNamePOJO.getWeather().indexOf(0) ;
-//    }
+    public int getWeatherID() {
+        return cityNamePOJO.getWeather().indexOf(0);
+    }
 
-public String getBase() {
+    //Clouds Getters
+    public long getClouds() {
+        return cityNamePOJO.getClouds().getAll();
+    }
+
+    // Base Getters
+    public String getBase() {
         return cityNamePOJO.getBase();
-}
+    }
 
+    // Main Getters
+    public double getFeelsLike() {
+        return cityNamePOJO.getMain().getFeelsLike();
+    }
 
+    public long getHumidity() {
+        return cityNamePOJO.getMain().getHumidity();
+    }
 
+    public long getPressure() {
+        return cityNamePOJO.getMain().getPressure();
+    }
 
+    public double getTemp() {
+        return cityNamePOJO.getMain().getTemp();
+    }
 
+    public double getTempMax() {
+        return cityNamePOJO.getMain().getTempMax();
+    }
+
+    public double getTempMin() {
+        return cityNamePOJO.getMain().getTempMin();
+    }
+
+    // Visibility Getter
+    public long getVisibility() {
+        return cityNamePOJO.getVisibility();
+    }
+
+    // Wind  Getters
+    public double getWindSpeed() {
+        return cityNamePOJO.getWind().getSpeed();
+    }
+
+    public long getWindDeg() {
+        return cityNamePOJO.getWind().getDeg();
+    }
+
+    public long getDT() {
+        return cityNamePOJO.getDt();
+    }
+
+    // Sys Getters
+    public long getSysType() {
+        return cityNamePOJO.getSys().getType();
+    }
+
+    public long getSysID() {
+        return cityNamePOJO.getSys().getId();
+    }
+
+    public String getSysCountry() {
+        return cityNamePOJO.getSys().getCountry();
+    }
+
+    public long getSysSunrise() {
+        return cityNamePOJO.getSys().getSunrise();
+    }
+
+    public long getSysSunset() {
+        return cityNamePOJO.getSys().getSunset();
+    }
+
+    public long getId() {
+        return cityNamePOJO.getId();
+    }
+
+    public long getCod() {
+        return cityNamePOJO.getCod();
+    }
+
+    public long getTimezone() {
+        return cityNamePOJO.getTimezone();
+    }
+
+    public String getName() {
+        return cityNamePOJO.getName();
+    }
 }
