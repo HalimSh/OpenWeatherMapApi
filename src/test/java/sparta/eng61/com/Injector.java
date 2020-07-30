4f663164e060ab1d5183fd6930eabbdc598d0948
lizzieedge21 <eedge@spartaglobal.com>
2020-07-30 10:50:10 +0100

    POJO commit

--- src/test/java/sparta/eng61/com/Injector.java
+++ src/test/java/sparta/eng61/com/Injector.java
@@ -1,22 +1,20 @@
 package sparta.eng61.com;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class Injector {
     static String url;
 
     public int getDTO(String path) {
         if (url.contains("q=") || url.contains("weather?id=")) {
-            DtoType cityNameDTO = DTOFactory.checkDTO(1);
+            DtoType cityNameDTO = POJOFactory.checkPOJO(1);
 
         } else if (url.contains("lat")) {
-            DtoType cityNameDTO = DTOFactory.checkDTO(2);
+            DtoType cityNameDTO = POJOFactory.checkPOJO(2);
 
         } else if (url.contains("group?id=")) {
-            DtoType cityNameDTO = DTOFactory.checkDTO(3);
+            DtoType cityNameDTO = POJOFactory.checkPOJO(3);
 
         } else if (url.contains("zip=")) {
-            DtoType cityNameDTO = DTOFactory.checkDTO(4);
+            DtoType cityNameDTO = POJOFactory.checkPOJO(4);
 
         }
 
