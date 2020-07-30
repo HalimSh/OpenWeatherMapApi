0fa4681466fe64a0071ff2952c0d849d4621b860
lizzieedge21 <eedge@spartaglobal.com>
2020-07-30 11:36:37 +0100

    Methods update

--- src/test/java/sparta/eng61/com/ConnectionManager.java
+++ src/test/java/sparta/eng61/com/ConnectionManager.java
@@ -23,10 +23,41 @@ public class ConnectionManager {
         return cityNamePOJO;
     }
 
-    public URL getCityNameURL (String cityName)
+    public String getCityNameURL (String cityName) {
+        return "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKEY;
+    }
 
-//    "api.openweathermap.org/data/2.5/weather?q=" + CityName "&appid="+ APIKey
+    public String getUSCityURL (String cityName, String stateCode) {
+        return "api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + stateCode + "&appid=" + APIKey;
+    }
 
+    public String getCityByCountryCodeURL(String cityName, String countryCode) {
+        return "api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + countryCode + "&appid=" + APIKey;
+    }
+
+    public String getCityIDURL(int cityID) {
+        return "api.openweathermap.org/data/2.5/weather?id=" + cityID + "&appid=" + APIKEY;
+    }
+
+    public String getCityByCoordinates (int lat, int lon) {
+        return "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&long=" + lon + "&appid=" + APIKEY;
+    }
+
+    public String getCityByZipCode (String zip, String countryCode) {
+        return "api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + countryCode  + "&appid=" + APIKEY;
+    }
+
+    public String getMultipleCitiesByZone (int topLeft, int bottomLeft, int bottomRight, int topRight, int zoom) {
+        return "api.openweathermap.org/data/2.5/box/city?bbox=" + topLeft + "," + bottomLeft + "," + bottomRight + "," + topRight + "," + "," + zoom;
+    }
+
+    public String getMultipleCitiesByCycle (int lat, int lon, int count) {
+        return "api.openweathermap.org/data/2.5/find?lat=" + lat + "&lon=" + lon + "&cnt=" + count;
+    }
+
+    public String getMutlipleCitiesByID (int cityID) {
+        return "api.openweathermap.org/data/2.5/find?id=" + cityID;
+    }
 
     public HttpURLConnection readAPIHeaders(String link) {
         try {
